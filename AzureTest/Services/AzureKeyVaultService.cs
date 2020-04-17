9aaa1b770e91e81f10f616dbf611301159f27a27
Dmitrii Petukhov (Akvelon INC) <v-dmpetu@microsoft.com>
2020-04-18 00:01:10 +0300

    Azure kay vault service

---
+++ AzureTest/Services/AzureKeyVaultService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace AzureTest.Services
+{
+    public class AzureKeyVaultService
+    {
+        private readonly IConfiguration configuration;
+        private readonly KeyVaultClient keyVaultClient;
+        public AzureKeyVaultService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            this.keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+        }
+
+        public string GetConnectionString(string connectionStringName)
+        {
+            var keyVaultSection = this.configuration.GetSection("KeyVault");
+            string keyVaultName = keyVaultSection["Name"];
+            string secretKey = keyVaultSection.GetSection("SecretKeys")[connectionStringName]; ;
+#if RELEASE
+            
+            string connectionString = this.keyVaultClient.GetSecretAsync($"https://{keyVaultName}.vault.azure.net/secrets/{secretKey}").Result.Value;
+#endif
+#if DEBUG
+            string connectionString = this.configuration.GetConnectionString(connectionStringName);
+#endif
+            return connectionString;
+        }
+    }
+}
